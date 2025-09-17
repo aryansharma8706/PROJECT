dc51d210218809f3dd9ea42dc396133cac1d05cf
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-17 11:59:05 +0530

    form changes

--- aryan/src/App.jsx
+++ aryan/src/App.jsx
@@ -141,17 +141,52 @@
 //create a button when i click on the button changes its text .
 //create two button when i click on a button swap both btns title ;
 
-function App(){
+import { useState } from 'react';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+  };
   return (
     <div>
-      <form>
-      <label>Email</label>
-      <input type="Email" placeholder="Enter your Email" name="email"></input>
-      <label>Password</label>
-      <input type="password" placeholder="password" name="password"></input>
-      <button>Submit</button>
+      <h1>FORM HANDLING---</h1>
+      <h1>
+        GETTING VALUES OUT OF THE FORM INPUT FIELDS AND SUBMIT THE VALUES AS AN
+        OBJECT
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input onChange={handleEmailChange} type="email" name="email" />
+        <label>Password</label>
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          name="password"
+        />
+        <button>Submit</button>
       </form>
+      <p>Email : {email}</p>
+      <p>Password : {password}</p>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
