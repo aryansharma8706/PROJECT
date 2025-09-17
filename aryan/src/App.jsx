9c57f207d6450245d7bbe5703a5079c247403db6
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-17 10:43:47 +0530

    updatesonclick

--- aryan/src/App.jsx
+++ aryan/src/App.jsx
@@ -72,28 +72,54 @@
 // }
 // export default App
 
-import { useState } from 'react';
+// import { useState } from 'react';
+// function App() {
+//   console.log(useState('REACT'));
+//   let [newTechnology, setNewTechnology] = useState('ANGULAR');
+//   console.log(newTechnology);
+
+//   const handleClick = () => {
+//     setNewTechnology('REACT');
+//   };
+//   return (
+//     <div>
+//       <h1>{newTechnology}</h1>
+//       <button onClick={handleClick}>Update</button>
+//     </div>
+//   );
+// }
+// export default App;
+
+//hooks  => js function
+
+//create three state variables like name , email , password ;
+// update all this at once when user click on a button
+
+import { useState } from 'react';   
 function App() {
   console.log(useState('REACT'));
   let [newTechnology, setNewTechnology] = useState('ANGULAR');
   console.log(newTechnology);
+ const [name , setName] = useState('TEST')
+ const [email,setEmail] = useState('test@gmail.com')
+ const [password ,setPassword] = useState('2421@123') ;
 
   const handleClick = () => {
     setNewTechnology('REACT');
   };
+  const handleUpdateClick = () => {
+    setName('NEW-TEST')
+    setPassword('12345')
+    setEmail('newtest@gmail.com')
+  }
   return (
     <div>
       <h1>{newTechnology}</h1>
+      <h1>{`${name} ${email} ${password}`}</h1>
+      <button onClick={handleUpdateClick}>Click to update</button>
       <button onClick={handleClick}>Update</button>
     </div>
   );
 }
+export default App
 
-export default App;
-
-//hooks  => js function
-
-
-//create three state variables like name , email , password ;
-
-// update all this at once when user click on a button
