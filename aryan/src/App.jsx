5d992a9dacd6e5d82e3f8ffbe01fa7a3e2c7d608
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-07 10:42:08 +0530

    increment

--- aryan/src/App.jsx
+++ aryan/src/App.jsx
@@ -1,21 +1,33 @@
-import Profile from './components/Profile';
-import Header from './components/Header';
-import React from 'react'
-
-import { useState } from 'react';
+import React from 'react';
+import { useReducer } from 'react';
 function App() {
-    const [username , setUsername] = useState("test@123")
-    console.log('app is running')
+  //first step create a initialState object
+  const initialState = {
+    count: 0,
+  };
+  //define a reducer function ,
+  //Reducer function takes  the state and action based on the action it updates the state variable
+  const reducer = (state, action) => {
+    console.log(state, action);
+    if (action.type === 'increment') {
+      return { count: state.count + 1 };
+    } else {
+      return state;
+    }
+  };
+  //step 3 pass both reducer and initial state as an argument in the usereducer hook
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state, dispatch);
+
+  const handleIncrement = () => {
+    dispatch({ type: 'increment' });
+  };
   return (
     <div>
-    <h1 className='text-center text-2xl font-bold m-4'>PROPS DRILLING</h1>
-
-    <Profile username={username}/>
-    <Header username={username}/>
-
-      <button className='px-10 py-4 bg-sky-500 rounded-xl text-white font-medium' onClick={()=>setUsername('newtest@123')}>Click</button>
+      <h1>{state.count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
