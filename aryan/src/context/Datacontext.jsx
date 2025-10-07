aaa455322f983881cae0bb710cd6fa57e7d3bb87
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-07 09:33:59 +0530

    context

---
+++ aryan/src/context/Datacontext.jsx
@@ -0,0 +1,18 @@
+//NOTE STEP1 IMPORT CREATECONTEXT FN FROM REACT ;
+import { createContext, useState } from 'react';
+
+//NOTE STEP2 call the createContext fn and save its result with in a variable name same as user context file and export it
+export const DataContext = createContext();
+
+//NOTE STEP3 Create a PROVIDER COMPONENT where you can define the data which you are going to share across your react application.
+export const DataProvider = ({ children }) => {
+  const [username, setUsername] = useState('Data shared using context api');
+  console.log(children);
+  const [email, setEmail] = useState('test@gmail.com');
+  return (
+    <DataContext.Provider value={{ username, email }}>
+      {children}
+      {/* here the value of children itself is app component = app */}
+    </DataContext.Provider>
+  );
+};
