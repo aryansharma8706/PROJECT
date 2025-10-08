29772604f2965ef3086fafd0dc0ab71bb416d81f
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-08 10:27:09 +0530

    no changes

--- aryan/src/redux/slices/CounterSlice.js
+++ aryan/src/redux/slices/CounterSlice.js
@@ -1,16 +1,18 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const counterSlice = createSlice({
-    name : "counter",
-    initialState : {count: 0},
+  name: 'counter',
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state, action) => {
+      return { count: state.count + 1 };
+    },
+    decrement: (state, action) => {
+      return { count: state.count - 1 };
+    },
+  },
+});
 
-    reducers : {
-        increment : (state,action) =>{
-            return {count : state.count +1};
-        },
-        decrement : (state,action) =>{
-            return {count : state.count -1};
-        }
-    }
-
-})
+//export reducer property from counterslice 
+export default counterSlice.reducer
+export const {increment,decrement} = counterSlice.actions
