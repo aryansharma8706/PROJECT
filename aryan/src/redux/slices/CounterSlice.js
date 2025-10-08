213c1af87585fabc71526c0d58768e90aadfc143
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-08 10:11:23 +0530

    `Added reducers to CounterSlice.js`

--- aryan/src/redux/slices/CounterSlice.js
+++ aryan/src/redux/slices/CounterSlice.js
@@ -3,6 +3,14 @@ import {createSlice} from "@reduxjs/toolkit"
 const counterSlice = createSlice({
     name : "counter",
     initialState : {count: 0},
-    
-    
+
+    reducers : {
+        increment : (state,action) =>{
+            return {count : state.count +1};
+        },
+        decrement : (state,action) =>{
+            return {count : state.count -1};
+        }
+    }
+
 })
