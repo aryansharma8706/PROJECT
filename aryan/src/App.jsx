68cd09f6b30def5e44615758085c80faa2db6a95
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-10-08 11:09:08 +0530

    increment slice

--- aryan/src/App.jsx
+++ aryan/src/App.jsx
@@ -95,55 +95,75 @@
 
 // export default App;
 
-import React, { useEffect } from 'react';
-import { useReducer } from 'react';
+// import React, { useEffect } from 'react';
+// import { useReducer } from 'react';
 
+// function App() {
+//   const initialState = {
+//     data: null,
+//     loading: false,
+//     error: null,
+//   };
+
+//   const reducer = (state, action) => {
+//     if (action.type === 'FETCH_PENDING') {
+//       return { ...state, loading: true };
+//     } else if (action.type === 'FETCH_SUCCESS') {
+//       console.log(action.payload);
+//       return { ...state, loading: false, data: action.payload };
+//     } else if (action.type === 'FETCH_FAILED') {
+//       return { ...state, error: action.payload, loading: false };
+//     } else {
+//       return state;
+//     }
+//   };
+
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   console.log(state);
+//   //NOTE Handling api calls,using useReducer hook
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         dispatch({ type: 'FETCH_PENDING' });
+//         const response = await fetch(
+//           'https://jsonplaceholder.typicode.com/todos'
+//         );
+//         const data = await response.json();
+
+//         dispatch({ type: 'FETCH_SUCCESS', payload: data });
+//       } catch (error) {
+//         dispatch({ type: 'FETCH_FAILED', payload: error.message });
+//       }
+//     };
+//     fetchData();
+//   }, []);
+//   return (
+//     <div>
+//       {state.loading && <div>Loading...</div>}
+//       {state.error && <div>{state.error}</div>}
+//       {state.data && !state.loading && <div>{JSON.stringify(state.data)}</div>}
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react'
+import { useSelector  , useDispatch} from 'react-redux'
+import { increment } from './redux/slices/CounterSlice'
 function App() {
-  const initialState = {
-    data: null,
-    loading: false,
-    error: null,
-  };
+const {count} = useSelector((state)=>state.count)
+const dispatch = useDispatch()
 
-  const reducer = (state, action) => {
-    if (action.type === 'FETCH_PENDING') {
-      return { ...state, loading: true };
-    } else if (action.type === 'FETCH_SUCCESS') {
-      console.log(action.payload);
-      return { ...state, loading: false, data: action.payload };
-    } else if (action.type === 'FETCH_FAILED') {
-      return { ...state, error: action.payload, loading: false };
-    } else {
-      return state;
-    }
-  };
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state);
-  //NOTE Handling api calls,using useReducer hook
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        dispatch({ type: 'FETCH_PENDING' });
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/todos'
-        );
-        const data = await response.json();
-
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
-      } catch (error) {
-        dispatch({ type: 'FETCH_FAILED', payload: error.message });
-      }
-    };
-    fetchData();
-  }, []);
+const handleIncrement = () => {
+  dispatch(increment())
+}
   return (
-    <div>
-      {state.loading && <div>Loading...</div>}
-      {state.error && <div>{state.error}</div>}
-      {state.data && !state.loading && <div>{JSON.stringify(state.data)}</div>}
+    <div className='font-bold text-3xl flex h-screen items-center justify-center flex-col'>
+      <h1>{count}</h1>
+       {/* //replace this with count state variable */}
+       <button  onClick={handleIncrement} className='px-10 py-4 bg-blue-300 text-white font-bold'>Increment</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
